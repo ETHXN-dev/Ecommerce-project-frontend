b80aa1e96769707855ba7060afeda7e5f0d64a52
ETHXN <migethanu@gmail.com>
2026-02-22 11:16:01 +0100

    Separated the reload payment summary into another useEffect

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -11,18 +11,24 @@ export function CheckoutPage({ cart, loadCart }) {
   const [paymentSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
+    const loadPaymentSummary = async () => {
+      let response = await axios.get("/api/payment-summary");
+      setPaymentSummary(response.data);
+    };
+
+    loadPaymentSummary();
+  }, [cart]);
+
+  useEffect(() => {
     const fetchCheckoutData = async () => {
       let response = await axios.get(
         "/api/delivery-options?expand=estimatedDeliveryTime",
       );
       setDeliveryOptions(response.data);
-
-      response = await axios.get("/api/payment-summary");
-      setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
-  }, [cart]);
+  }, []);
 
   return (
     <>
