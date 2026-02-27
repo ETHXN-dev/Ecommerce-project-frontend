8d8710fdfc3431f3e305e1d8a99cfda901e17fe6
ETHXN <migethanu@gmail.com>
2026-02-28 00:02:31 +0100

    Added ability to search for products

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router";
+import { useState } from "react";
+import { NavLink, useNavigate, useSearchParams } from "react-router";
 import LogoWhite from "../assets/images/logo-white.png";
 import MobileLogoWhite from "../assets/images/mobile-logo-white.png";
 import CartIcon from "../assets/images/icons/cart-icon.png";
@@ -6,6 +7,22 @@ import SearchIcon from "../assets/images/icons/search-icon.png";
 import "./Header.css";
 
 export function Header({ cart }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const searchText = searchParams.get("search");
+
+  const [search, setSearch] = useState(searchText || "");
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    console.log(search);
+    navigate(`/?search=${search}`);
+  };
+
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
@@ -23,9 +40,15 @@ export function Header({ cart }) {
         </div>
 
         <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={updateSearchInput}
+          />
 
-          <button className="search-button">
+          <button className="search-button" onClick={searchProducts}>
             <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,23 +1,29 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
 import { Header } from "../../components/Header";
 import { ProductsGrid } from "./ProductsGrid";
 import "./HomePage.css";
 
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
 
   // async is made as another function becuase it returns a promise and useEffect cant return a promise
   useEffect(() => {
     // use effect can't return a promise
     const getHomeData = async () => {
-      const response = await axios.get("/api/products");
+      const urlPath = search
+        ? `/api/products?search=${search}`
+        : "/api/products";
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
 
     // Call the new function
     getHomeData();
-  }, []);
+  }, [search]);
 
   return (
     <>
